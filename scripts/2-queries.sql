9ea78f85029f2cc0059254d921a044f2c4c310e2
Kamil <kamil.figon@gmail.com>
2020-04-09 00:34:23 +0200

    more queries

--- scripts/2-queries.sql
+++ scripts/2-queries.sql
@@ -4,4 +4,11 @@ select first_name, last_name from employees;
 select * from employees
 where gender='M' and first_name='Denis';
 
+# sql wykonuje najpierw AND, potem OR, wiec zlapalo wszystkie kobiety
+# kolejnosc warunkow nie ma znaczenia
+select * from employees
+where first_name='Denis' and gender='M' or gender='F';
+
+select * from employees
+where first_name IN ('Cathie', 'Mark', 'Nathan');
 
