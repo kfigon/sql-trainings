2569f184c3db968d4c69d97d13cd1b37bde9f604
Kamil <kamil.figon@gmail.com>
2020-04-09 22:41:23 +0200

    more queries

--- scripts/5-more-aggregates.sql
+++ scripts/5-more-aggregates.sql
@@ -32,6 +32,7 @@ from departments_duplicate;
 # ifnull == coalesce z dwoma parametrami
 # coalesce - ifnull with more parameters allowed
 # will provide single value, first non null value from list (from left to right)
+#  NIE ZMIENIA DANYCH
 select dept_no,
        coalesce(dept_name, 'Department name not provided') as dept_name
 from departments_duplicate;
@@ -39,5 +40,10 @@ from departments_duplicate;
 select dept_no,
        dept_name, # not needed for working, just for visualisation
        coalesce(dept_manager, dept_name, 'N/A') as dept_manager
+# takes dept_manager, if null, then dept_name, if null - N/A
+from departments_duplicate;
+
+select dept_name, dept_no,
+       coalesce('department manager value') as fake_column
 from departments_duplicate;
 
