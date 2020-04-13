b492f97ddb375733a64a638ff6d0e850aa2dffa3
Kamil <kamil.figon@gmail.com>
2020-04-13 15:36:55 +0200

    more sub querues

--- scripts/8-subqueries.sql
+++ scripts/8-subqueries.sql
@@ -1,3 +1,5 @@
+# sql odpala inner, potem outer
+
 # subsquery, inner query, nested query, inner select
 # outer query/outer select
 select * from dept_manager;
@@ -9,3 +11,18 @@ where e.emp_no IN (
     select dm.emp_no
     from dept_manager dm
 );
+
+# Extract the information about all
+# department managers who were hired between the
+# 1st of January 1990 and the 1st of January 1995.
+select e.emp_no, e.last_name, e.first_name, e.hire_date
+from dept_manager dm
+join employees e on dm.emp_no = e.emp_no
+where e.hire_date between '1990-01-01' and '1995-01-01';
+
+select *
+from dept_manager dm
+where dm.emp_no IN (
+        select e2.emp_no from employees e2
+        where e2.hire_date between '1990-01-01' and '1995-01-01'
+    )
