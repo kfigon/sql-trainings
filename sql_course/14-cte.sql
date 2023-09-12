f5b78762b8ad2e902b300b52bba73cbecb6cc746
kfigon <kamil.figon@gmail.com>
2023-09-12 19:14:58 +0200

    cte 2

--- sql_course/14-cte.sql
+++ sql_course/14-cte.sql
@@ -29,4 +29,30 @@ select count(*)
 from employees e
 join salaries s on e.emp_no = s.emp_no and e.gender = 'F'
 cross join avg_salary
-where avg_salary.avg_salary_val <= s.salary
+where avg_salary.avg_salary_val <= s.salary;
+
+
+# find out how many male employees have never signed a contract with a
+# salary value higher than or equal to the all-time company salary average
+with avg_salary as (select avg(salary) as avg_salary_val from salaries)
+select count(*)
+from employees e
+join salaries s on e.emp_no = s.emp_no and e.gender = 'M'
+cross join avg_salary
+where s.salary < avg_salary.avg_salary_val;
+
+# how many female employees highest contract salary were higher than
+# all time company salary average?
+
+# 2 CTEs
+with avg_salary as (select avg(salary) as avg_salary_val from salaries),
+highes_contract_of_females as (
+    select e.emp_no, max(s.salary) as salary
+    from employees e
+    join salaries s on e.emp_no = s.emp_no and e.gender = 'F'
+    group by e.emp_no
+)
+select count(*)
+from highes_contract_of_females
+cross join avg_salary
+where highes_contract_of_females.salary > avg_salary.avg_salary_val
